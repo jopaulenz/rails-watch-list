42619b878e4a1b5ed9e27ebf9dd70223ca2463b7
Josephine Paulenz <josephinepaulenz@gmail.com>
2024-08-15 16:33:08 +0200

    update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,22 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     @bookmark = Bookmark.new
   end
+  def new
+    @list = List.new
+  end
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+
+  end
 end
